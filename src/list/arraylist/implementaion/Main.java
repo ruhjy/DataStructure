28b8a9fe9454cea6981ca03293d18e0787df16cd
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-26 01:15:26 +0900

    ArrayList test
    add, remove, get, indexOf 테스트

--- src/list/arraylist/implementaion/Main.java
+++ src/list/arraylist/implementaion/Main.java
@@ -19,6 +19,23 @@ public class Main {
 
 		System.out.println(numbers.remove(2));
 		System.out.println(numbers);
+		System.out.println("========");
+
+		System.out.println(numbers.removeFirst());
+		System.out.println(numbers.removeLast());
+		System.out.println(numbers); // 20,40,50,60,70 size 5
+		System.out.println("========");
+
+		System.out.println(numbers.get(1)); // 40
+		System.out.println("========");
+
+		System.out.println("size = " + numbers.size()); // 5
+		System.out.println(numbers.indexOf(50)); // 2
+		System.out.println(numbers.indexOf(20)); // 0
+		System.out.println(numbers.indexOf(70)); // 4
+		System.out.println(numbers.indexOf(10)); // -1
+		System.out.println("========");
+
 	}
 
 }
