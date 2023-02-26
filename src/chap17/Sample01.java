8e1a438352bd56b3a8d0f99c2b9369369201e2a2
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-02-26 23:53:16 +0900

    빵형의 실전 - 스트림

--- src/chap17/Sample01.java
+++ src/chap17/Sample01.java
@@ -6,8 +6,8 @@ import java.util.HashSet;
 
 public class Sample01 {
 	public static void main(String[] args) {
-	
-		var list = Arrays.asList(1,2,3,2,1,5);
+
+		var list = Arrays.asList(1, 2, 3, 2, 1, 5);
 		System.out.println(list);
 		var<Integer> list2 = new ArrayList<>(new HashSet<Integer>(list));
 		list2.forEach(System.out::println);
