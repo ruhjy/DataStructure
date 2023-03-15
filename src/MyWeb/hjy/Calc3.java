fb9ef5192561eeccbf2b4b2e07aad9bdff5a1382
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-03-15 22:18:02 +0900

    jsp&servlet

--- src/MyWeb/hjy/Calc3.java
+++ src/MyWeb/hjy/Calc3.java
@@ -2,13 +2,16 @@ package com.hjy;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/calc3")
 public class Calc3 extends HttpServlet {
@@ -17,35 +20,39 @@ public class Calc3 extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		ServletContext application = request.getServletContext();
-		HttpSession session = request.getSession();
-		
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
-		
-		String v_ = request.getParameter("v");
-		String op = request.getParameter("operator");
-		
-		int v = 0, result = 0;
-		if(v_ != null && !v_.equals("")) {
-			v = Integer.parseInt(v_);
-		}
-		
-		if(op.equals("=")) {
-			int x = (Integer)session.getAttribute("value");
-			int y = v;
-			String operator = (String)session.getAttribute("operator");
-			if(operator.equals("+")) {
-				result = x + y;
-			} else {
-				result = x - y;
-			}
-			response.getWriter().println("result = " + result);
-		} else {
-			session.setAttribute("value", v);
-			session.setAttribute("operator", op);
-		}
-		
+		Cookie[] cookies = request.getCookies();
 
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+
+		String exp = "";
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if (c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+			}
+		}
+
+		if (operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			exp += (value == null) ? "" : value;
+			exp += (operator == null) ? "" : operator;
+			exp += (dot == null) ? "" : dot;
+		}
+
+		Cookie expCookie = new Cookie("exp", exp);
+
+		response.addCookie(expCookie);
+		response.sendRedirect("calcpage");
 	}
 }
